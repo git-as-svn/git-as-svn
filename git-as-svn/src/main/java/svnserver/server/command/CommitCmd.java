4ed694ed8fce365f5011f84f630749ed781a1956
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2014-08-13 18:42:58 +0400

    Fake svn commit for simple file change.

--- git-as-svn/src/main/java/svnserver/server/command/CommitCmd.java
+++ git-as-svn/src/main/java/svnserver/server/command/CommitCmd.java
@@ -3,6 +3,7 @@ package svnserver.server.command;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import svnserver.StringHelper;
 import svnserver.SvnConstants;
 import svnserver.parser.MessageParser;
 import svnserver.parser.SvnServerParser;
@@ -13,6 +14,7 @@ import svnserver.server.SessionContext;
 import svnserver.server.error.ClientErrorException;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -80,11 +82,42 @@ public class CommitCmd extends BaseCmd<CommitCmd.CommitParams> {
 
     public EditorPipeline(@NotNull CommitParams params) {
       commands = new HashMap<>();
+      commands.put("open-root", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("open-file", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("close-dir", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("close-file", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("textdelta-chunk", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("textdelta-end", new LambdaCmd<>(NoParams.class, this::fake));
+      commands.put("apply-textdelta", new LambdaCmd<>(NoParams.class, this::fake));
       commands.put("close-edit", new LambdaCmd<>(NoParams.class, this::closeEdit));
     }
 
-    private void closeEdit(@NotNull SessionContext context, @NotNull NoParams args) {
+    @Deprecated
+    private void fake(@NotNull SessionContext context, @NotNull NoParams args) {
+      context.push(this::editorCommand);
+    }
+
+    private void closeEdit(@NotNull SessionContext context, @NotNull NoParams args) throws IOException {
+      final SvnServerWriter writer = context.getWriter();
+      writer
+          .listBegin()
+          .word("success")
+          .listBegin()
+          .listEnd()
+          .listEnd();
+      sendError(writer, 0, "test");
       //context.push(new CheckPermissionStep(this::complete));
+    }
+
+    private void complete(@NotNull SessionContext context) throws IOException {
+      final SvnServerWriter writer = context.getWriter();
+      writer
+          .listBegin()
+          .number(42) // rev number
+          .listBegin().string(StringHelper.formatDate(new Date().getTime())).listEnd() // date
+          .listBegin().string("commit author").listEnd()
+          .listBegin().listEnd()
+          .listEnd();
     }
 
     private void editorCommand(@NotNull SessionContext context) throws IOException, ClientErrorException {
