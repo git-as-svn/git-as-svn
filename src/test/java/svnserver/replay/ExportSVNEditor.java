05e14c91b05f348eef09a5f44389da2fb4c17510
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2014-08-22 12:08:56 +0400

    Add revision compare.

---
+++ src/test/java/svnserver/replay/ExportSVNEditor.java
@@ -0,0 +1,125 @@
+package svnserver.replay;
+
+import org.jetbrains.annotations.NotNull;
+import org.tmatesoft.svn.core.SVNCommitInfo;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNPropertyValue;
+import org.tmatesoft.svn.core.io.ISVNEditor;
+import org.tmatesoft.svn.core.io.diff.SVNDiffWindow;
+
+import java.io.OutputStream;
+import java.util.*;
+
+/**
+ * ISVNEditor for comparing revisions from two repositories.
+ *
+ * @author a.navrotskiy
+ */
+public class ExportSVNEditor implements ISVNEditor {
+  @NotNull
+  private final Deque<String> paths = new ArrayDeque<>();
+  @NotNull
+  private final Map<String, String> files = new TreeMap<>();
+  @NotNull
+  private final Map<String, Map<String, String>> properties = new HashMap<>();
+
+  @Override
+  public void targetRevision(long revision) throws SVNException {
+  }
+
+  @Override
+  public void openRoot(long revision) throws SVNException {
+    paths.push("/");
+    files.put("/", "dir");
+  }
+
+  @Override
+  public void deleteEntry(String path, long revision) throws SVNException {
+  }
+
+  @Override
+  public void absentDir(String path) throws SVNException {
+  }
+
+  @Override
+  public void absentFile(String path) throws SVNException {
+  }
+
+  @Override
+  public void addDir(String path, String copyFromPath, long copyFromRevision) throws SVNException {
+    paths.push(path);
+    files.put(path, "directory");
+  }
+
+  @Override
+  public void openDir(String path, long revision) throws SVNException {
+    paths.push(path);
+    files.put(path, "directory");
+  }
+
+  @Override
+  public void changeDirProperty(String name, SVNPropertyValue value) throws SVNException {
+    properties.computeIfAbsent(paths.element(), (s) -> new TreeMap<>()).put(name, value.getString());
+  }
+
+  @Override
+  public void closeDir() throws SVNException {
+    paths.pop();
+  }
+
+  @Override
+  public void addFile(String path, String copyFromPath, long copyFromRevision) throws SVNException {
+  }
+
+  @Override
+  public void openFile(String path, long revision) throws SVNException {
+  }
+
+  @Override
+  public void changeFileProperty(String path, String name, SVNPropertyValue value) throws SVNException {
+    properties.computeIfAbsent(paths.element(), (s) -> new TreeMap<>()).put(name, value.getString());
+  }
+
+  @Override
+  public void closeFile(String path, String textChecksum) throws SVNException {
+    files.put(path, textChecksum);
+  }
+
+  @Override
+  public SVNCommitInfo closeEdit() throws SVNException {
+    return null;
+  }
+
+  @Override
+  public void abortEdit() throws SVNException {
+  }
+
+  @Override
+  public void applyTextDelta(String path, String baseChecksum) throws SVNException {
+  }
+
+  @Override
+  public OutputStream textDeltaChunk(String path, SVNDiffWindow diffWindow) throws SVNException {
+    return null;
+  }
+
+  @Override
+  public void textDeltaEnd(String path) throws SVNException {
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Files:\n");
+    for (Map.Entry<String, String> entry : files.entrySet()) {
+      sb.append("  ").append(entry.getKey()).append(" (").append(entry.getValue()).append(")\n");
+      final Map<String, String> props = properties.get(entry.getKey());
+      if (props != null) {
+        for (Map.Entry<String, String> prop : props.entrySet()) {
+          sb.append("    ").append(prop.getKey()).append(" = \"").append(prop.getValue()).append("\"\n");
+        }
+      }
+    }
+    return sb.toString();
+  }
+}
--- src/test/java/svnserver/replay/ReplayTest.java
+++ src/test/java/svnserver/replay/ReplayTest.java
@@ -3,14 +3,18 @@ package svnserver.replay;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.tmatesoft.svn.core.*;
-import org.tmatesoft.svn.core.io.*;
-import org.tmatesoft.svn.core.io.diff.SVNDiffWindow;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNPropertyValue;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.io.ISVNEditor;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import svnserver.parser.SvnTestServer;
 
 import java.io.File;
-import java.io.OutputStream;
 import java.net.URL;
 
 /**
@@ -34,7 +38,11 @@ public class ReplayTest {
       long lastRevision = srcRepo.getLatestRevision();
       log.info("Start replay");
       for (long revision = 2; revision <= lastRevision; revision++) {
+        final SVNPropertyValue message = srcRepo.getRevisionPropertyValue(revision, "svn:log");
+        log.info("  replay commit #{}: {}", revision, message.getString());
         replayRevision(srcRepo, dstRepo, revision);
+        log.info("  compare revisions #{}: {}", revision, message.getString());
+        compareRevision(srcRepo, dstRepo, revision);
       }
       log.info("End replay");
     }
@@ -43,117 +51,26 @@ public class ReplayTest {
   private void replayRevision(@NotNull SVNRepository srcRepo, @NotNull SVNRepository dstRepo, long revision) throws SVNException {
     final SVNPropertyValue message = srcRepo.getRevisionPropertyValue(revision, "svn:log");
     final ISVNEditor editor = dstRepo.getCommitEditor(message.getString(), null);
-    log.info("  replay commit #{}: {}", revision, message.getString());
-    srcRepo.diff(srcRepo.getLocation(), revision, revision - 1, null, false, SVNDepth.INFINITY, true, new ISVNReporterBaton() {
-      @Override
-      public void report(ISVNReporter reporter) throws SVNException {
-        reporter.setPath("", null, revision - 1, SVNDepth.INFINITY, false);
-        reporter.finishReport();
-      }
+    srcRepo.diff(srcRepo.getLocation(), revision, revision - 1, null, false, SVNDepth.INFINITY, true, reporter -> {
+      reporter.setPath("", null, revision - 1, SVNDepth.INFINITY, false);
+      reporter.finishReport();
     }, new FilterSVNEditor(editor));
-
   }
 
-  private static class FilterSVNEditor implements ISVNEditor {
-    @NotNull
-    private final ISVNEditor editor;
+  private void compareRevision(@NotNull SVNRepository srcRepo, @NotNull SVNRepository dstRepo, long revision) throws SVNException {
+    final ExportSVNEditor srcExport = new ExportSVNEditor();
+    srcRepo.diff(srcRepo.getLocation(), revision, revision - 1, null, false, SVNDepth.INFINITY, true, reporter -> {
+      reporter.setPath("", null, 0, SVNDepth.INFINITY, true);
+      reporter.finishReport();
+    }, new FilterSVNEditor(srcExport));
 
-    public FilterSVNEditor(@NotNull ISVNEditor editor) {
-      this.editor = editor;
-    }
+    final ExportSVNEditor dstExport = new ExportSVNEditor();
+    dstRepo.diff(srcRepo.getLocation(), revision, revision - 1, null, false, SVNDepth.INFINITY, true, reporter -> {
+      reporter.setPath("", null, 0, SVNDepth.INFINITY, true);
+      reporter.finishReport();
+    }, new FilterSVNEditor(dstExport));
 
-    @Override
-    public void targetRevision(long revision) throws SVNException {
-      editor.targetRevision(revision);
-    }
-
-    @Override
-    public void openRoot(long revision) throws SVNException {
-      editor.openRoot(revision);
-    }
-
-    @Override
-    public void deleteEntry(String path, long revision) throws SVNException {
-      editor.deleteEntry(path, revision);
-    }
-
-    @Override
-    public void absentDir(String path) throws SVNException {
-      editor.absentDir(path);
-    }
-
-    @Override
-    public void absentFile(String path) throws SVNException {
-      editor.absentFile(path);
-    }
-
-    @Override
-    public void addDir(String path, String copyFromPath, long copyFromRevision) throws SVNException {
-      editor.addDir(path, copyFromPath, copyFromRevision);
-    }
-
-    @Override
-    public void openDir(String path, long revision) throws SVNException {
-      editor.openDir(path, revision);
-    }
-
-    @Override
-    public void changeDirProperty(String name, SVNPropertyValue value) throws SVNException {
-      if (!name.startsWith(SVNProperty.SVN_ENTRY_PREFIX)) {
-        editor.changeDirProperty(name, value);
-      }
-    }
-
-    @Override
-    public void closeDir() throws SVNException {
-      editor.closeDir();
-    }
-
-    @Override
-    public void addFile(String path, String copyFromPath, long copyFromRevision) throws SVNException {
-      editor.addFile(path, copyFromPath, copyFromRevision);
-    }
-
-    @Override
-    public void openFile(String path, long revision) throws SVNException {
-      editor.openFile(path, revision);
-    }
-
-    @Override
-    public void changeFileProperty(String path, String propertyName, SVNPropertyValue propertyValue) throws SVNException {
-      if (!propertyName.startsWith(SVNProperty.SVN_ENTRY_PREFIX)) {
-        editor.changeFileProperty(path, propertyName, propertyValue);
-      }
-    }
-
-    @Override
-    public void closeFile(String path, String textChecksum) throws SVNException {
-      editor.closeFile(path, textChecksum);
-    }
-
-    @Override
-    public SVNCommitInfo closeEdit() throws SVNException {
-      return editor.closeEdit();
-    }
-
-    @Override
-    public void abortEdit() throws SVNException {
-      editor.abortEdit();
-    }
-
-    @Override
-    public void applyTextDelta(String path, String baseChecksum) throws SVNException {
-      editor.applyTextDelta(path, baseChecksum);
-    }
-
-    @Override
-    public OutputStream textDeltaChunk(String path, SVNDiffWindow diffWindow) throws SVNException {
-      return editor.textDeltaChunk(path, diffWindow);
-    }
-
-    @Override
-    public void textDeltaEnd(String path) throws SVNException {
-      editor.textDeltaEnd(path);
-    }
+    Assert.assertEquals(srcExport.toString(), dstExport.toString());
   }
+
 }
