d2795f0c54eee1e098123691418ea2b2f6e170d1
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2014-08-22 08:29:41 +0400

    Rename revision property git:sha1 to git-commit (like on github).

--- src/main/java/svnserver/SvnConstants.java
+++ src/main/java/svnserver/SvnConstants.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public final class SvnConstants {
   @NotNull
-  public static final String PROP_GIT = "git:sha1";
+  public static final String PROP_GIT = "git-commit";
   @NotNull
   public static final String LINK_PREFIX = "link ";
   @NotNull
