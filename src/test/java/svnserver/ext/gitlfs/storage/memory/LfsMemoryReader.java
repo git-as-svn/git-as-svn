2d030bc15a44fc4c7176857e315d75713c6d7686
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-07-13 21:58:33 +0300

    Add in-memory LFS storage

---
+++ src/test/java/svnserver/ext/gitlfs/storage/memory/LfsMemoryReader.java
@@ -0,0 +1,54 @@
+/**
+ * This file is part of git-as-svn. It is subject to the license terms
+ * in the LICENSE file found in the top-level directory of this distribution
+ * and at http://www.gnu.org/licenses/gpl-2.0.html. No part of git-as-svn,
+ * including this file, may be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+package svnserver.ext.gitlfs.storage.memory;
+
+import org.apache.commons.codec.binary.Hex;
+import org.jetbrains.annotations.NotNull;
+import svnserver.ext.gitlfs.storage.LfsReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Local storage writer.
+ *
+ * @author Artem V. Navrotskiy <bozaro@users.noreply.github.com>
+ */
+public class LfsMemoryReader implements LfsReader {
+  @NotNull
+  private final byte[] content;
+
+  public LfsMemoryReader(@NotNull byte[] content) {
+    this.content = content;
+  }
+
+  @NotNull
+  @Override
+  public InputStream openStream() throws IOException {
+    return new ByteArrayInputStream(content);
+  }
+
+  @Override
+  public long getSize() {
+    return content.length;
+  }
+
+  @NotNull
+  @Override
+  public String getMd5() {
+    try {
+      return Hex.encodeHexString(MessageDigest.getInstance("MD5").digest(content));
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+}
