17fe94f5217ac7d40205fcb82e39aecf72dba6ac
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-10-10 23:06:03 +0300

    Add script for create git user/group on installation

---
+++ deploy/deb/DEBIAN/preinst.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+# creating git group if he isn't already there
+if ! getent group git >/dev/null; then
+ 	# Adding system group: git
+	addgroup --system git >/dev/null
+fi
+
+# creating git user if he isn't already there
+if ! getent passwd git >/dev/null; then
+	# Adding system user: git
+	adduser \
+	  --disabled-login \
+	  --ingroup git \
+	  --home /home/git \
+	  --shell /bin/bash \
+	  --gecos git \
+	  git > /dev/null
+fi
