8126bd60e3b3fec3051cf6db7c4d0db2993e0d26
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-10-27 19:01:43 +0300

    API: Add text protobuf format support

--- src/main/java/svnserver/ext/api/formatter/BaseFormat.java
+++ src/main/java/svnserver/ext/api/formatter/BaseFormat.java
@@ -9,7 +9,7 @@ package svnserver.ext.api.formatter;
 
 import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.Message;
-import com.googlecode.protobuf.format.AbstractCharBasedFormatter;
+import com.googlecode.protobuf.format.ProtobufFormatter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import svnserver.ext.api.ProtobufFormat;
@@ -17,6 +17,8 @@ import svnserver.ext.api.ProtobufFormat;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * JSON serialization.
@@ -25,22 +27,26 @@ import java.io.IOException;
  */
 public abstract class BaseFormat extends ProtobufFormat {
   @NotNull
-  private final AbstractCharBasedFormatter format;
+  private final ProtobufFormatter formatter;
 
-  public BaseFormat(@NotNull AbstractCharBasedFormatter format, @NotNull String mimeType, @NotNull String suffix) {
+  public BaseFormat(@NotNull ProtobufFormatter formatter, @NotNull String mimeType, @NotNull String suffix) {
     super(mimeType, suffix);
-    this.format = format;
+    this.formatter = formatter;
   }
 
   @Override
   public void write(@NotNull Message message, @NotNull HttpServletResponse output) throws IOException {
-    format.print(message, output.getWriter());
+    formatter.print(message, output.getOutputStream(), getCharset(output.getCharacterEncoding()));
   }
 
   @Override
   @Nullable
   public Message read(@NotNull Message.Builder builder, @NotNull HttpServletRequest input) throws IOException {
-    format.merge(input.getReader(), ExtensionRegistry.getEmptyRegistry(), builder);
+    formatter.merge(input.getInputStream(), getCharset(input.getCharacterEncoding()), ExtensionRegistry.getEmptyRegistry(), builder);
     return builder.build();
+  }
+
+  private Charset getCharset(@Nullable String charset) {
+    return charset == null ? StandardCharsets.ISO_8859_1 : Charset.forName(charset);
   }
 }
