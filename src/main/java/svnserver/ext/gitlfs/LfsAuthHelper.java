d92a595d0845bc7af3bfb06a618880fb3b0b926d
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-11-16 08:47:22 +0300

    API: Implement RPC method for git-lfs-authenticate

--- src/main/java/svnserver/ext/gitlfs/LfsAuthHelper.java
+++ src/main/java/svnserver/ext/gitlfs/LfsAuthHelper.java
@@ -32,17 +32,16 @@ import java.util.Date;
  * @author Artem V. Navrotskiy <bozaro@users.noreply.github.com>
  */
 public class LfsAuthHelper {
-
   public static Link createToken(
       @NotNull SharedContext context,
-      @NotNull URI uri,
+      @NotNull URI baseLfsUrl,
       @Nullable String username,
       @Nullable String external,
       boolean anonymous
-  ) throws ServerError, IOException {
-    final UserDB userDB = context.sure(UserDB.class);
-    final User user;
+  ) throws ServerError {
     try {
+      final UserDB userDB = context.sure(UserDB.class);
+      final User user;
       if (anonymous) {
         user = User.getAnonymous();
       } else if (external == null && username == null) {
@@ -50,26 +49,33 @@ public class LfsAuthHelper {
       } else {
         user = username != null ? userDB.lookupByUserName(username) : userDB.lookupByExternal(external);
       }
+      if (user == null) {
+        //throw new NotFoundException();
+        throw new ServerError(HttpServletResponse.SC_NOT_FOUND, "User not found");
+      }
+      return createToken(context, baseLfsUrl, user);
     } catch (SVNException | IOException e) {
-      throw new ServerError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Can't get user information. See log for more details", e);
+      throw new ServerError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Can't get user information. See server log for more details", e);
     }
-    if (user == null) {
-      //throw new NotFoundException();
-      throw new ServerError(HttpServletResponse.SC_NOT_FOUND, "User not found");
-    }
+  }
 
+  public static Link createToken(
+      @NotNull SharedContext context,
+      @NotNull URI baseLfsUrl,
+      @NotNull User user
+  ) throws IOException {
     // Calculate expire time and token.
     NumericDate expireAt = NumericDate.now();
     expireAt.addSeconds(60);
-    final String accessToken = TokenHelper.createToken(WebServer.get(context).createEncryption(), user, expireAt);
+    WebServer webServer = WebServer.get(context);
+    final String accessToken = TokenHelper.createToken(webServer.createEncryption(), user, expireAt);
 
     return new Link(
-        uri,
+        baseLfsUrl,
         ImmutableMap.<String, String>builder()
             .put(Constants.HEADER_AUTHORIZATION, WebServer.AUTH_TOKEN + accessToken)
             .build(),
         new Date(expireAt.getValueInMillis())
     );
   }
-
 }
