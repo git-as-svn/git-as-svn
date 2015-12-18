53ea17fe88263ad7bd9ea8c438c22503ebd61755
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-12-18 22:47:13 +0300

    Docbook: Add Russian translation

--- docbook/update.sh
+++ docbook/update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -xe
 for po in *.po; do
     xml2po -u $po base/*.xml
+    target=../build/docbook/${po/.po/}
+    rm -fR $target
+    mkdir -p `dirname $target`
+    cp -R base/ $target
+    for xml in base/*.xml; do
+        xml2po -p $po $xml > $target/`basename $xml`
+    done
 done
 
