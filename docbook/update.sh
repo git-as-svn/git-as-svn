2bfccab37ff61b70985217cfac3a6c9d2b0fa0e8
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-12-19 21:36:44 +0300

    Docbook: Add .PDF generation

--- docbook/update.sh
+++ docbook/update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -xe
+cd `dirname $0`
+
 for po in *.po; do
+    lang=${po/.po/}
     xml2po -u $po base/*.xml
-    target=../build/docbook/${po/.po/}
+    target=../build/docbook.$lang
     rm -fR $target
     mkdir -p `dirname $target`
     cp -R base/ $target
     for xml in base/*.xml; do
         xml2po -p $po $xml > $target/`basename $xml`
     done
+    fop -xml $target/manual.xml -xsl /usr/share/sgml/docbook/stylesheet/xsl/docbook-xsl/fo/docbook.xsl -pdf $target/../manual.$lang.pdf
 done
 
