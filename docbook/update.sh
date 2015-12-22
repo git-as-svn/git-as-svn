15dcbdacf72a1d989b1981c33d63f6b4be9f930a
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-12-22 21:52:36 +0300

    Docbook: Add some docbook parameters

--- docbook/update.sh
+++ docbook/update.sh
@@ -11,6 +11,15 @@ for po in *.po; do
     for xml in base/*.xml; do
         xml2po -p $po $xml > $target/`basename $xml`
     done
-    fop -c fop.xml -xml $target/manual.xml -xsl /usr/share/xml/docbook/stylesheet/docbook-xsl/fo/docbook.xsl -pdf $target/../distributions/manual.$lang.pdf
+    docxsl=/usr/share/xml/docbook/stylesheet/docbook-xsl
+    fop -c fop.xml \
+        -xml $target/manual.xml \
+        -xsl $docxsl/fo/docbook.xsl \
+        -param fop1.extensions 1 \
+        -param admon.graphics 1 \
+        -param admon.graphics.extension .svg \
+        -param admon.graphics.path $docxsl/images/ \
+        -param highlight.source 1 \
+        -pdf $target/../distributions/manual.$lang.pdf
 done
 
