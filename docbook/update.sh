8486c2254fb3f3255c0acfbd1f7852325ca6adeb
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-12-23 07:27:03 +0300

    Docbook: Begin generation .PDF with gradle

--- docbook/update.sh
+++ docbook/update.sh
@@ -3,23 +3,23 @@ cd `dirname $0`
 
 for po in *.po; do
     lang=${po/.po/}
-    xml2po -u $po base/*.xml
-    target=../build/docbook.$lang
+    xml2po -u $po src/reference/*.xml
+    target=../build/docbook/$lang
     rm -fR $target
     mkdir -p `dirname $target`
-    cp -R base/ $target
-    for xml in base/*.xml; do
+    cp -R src/reference/ $target
+    for xml in src/reference/*.xml; do
         xml2po -p $po $xml > $target/`basename $xml`
     done
     docxsl=/usr/share/xml/docbook/stylesheet/docbook-xsl
-    fop -c fop.xml \
-        -xml $target/manual.xml \
+    fop -c src/fonts/fop.xml \
+        -xml $target/index.xml \
         -xsl $docxsl/fo/docbook.xsl \
         -param fop1.extensions 1 \
         -param admon.graphics 1 \
         -param admon.graphics.extension .svg \
         -param admon.graphics.path $docxsl/images/ \
         -param highlight.source 1 \
-        -pdf $target/../distributions/manual.$lang.pdf
+        -pdf $target/../../distributions/manual.$lang.pdf
 done
 
