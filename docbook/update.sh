40a9f319aeaba1dc0df10e6f96f2a90cd7331917
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-12-24 11:35:00 +0300

    Docbook: Generate .PDF with gradle

--- docbook/update.sh
+++ docbook/update.sh
@@ -1,25 +1,8 @@
 #!/bin/bash -xe
 cd `dirname $0`
 
-for po in *.po; do
-    lang=${po/.po/}
-    xml2po -u $po src/reference/*.xml
-    target=../build/docbook/$lang
-    rm -fR $target
-    mkdir -p `dirname $target`
-    cp -R src/reference/ $target
-    for xml in src/reference/*.xml; do
-        xml2po -p $po $xml > $target/`basename $xml`
-    done
-    docxsl=/usr/share/xml/docbook/stylesheet/docbook-xsl
-    fop -c fop-userconfig.xml \
-        -xml $target/index.xml \
-        -xsl $docxsl/fo/docbook.xsl \
-        -param fop1.extensions 1 \
-        -param admon.graphics 1 \
-        -param admon.graphics.extension .svg \
-        -param admon.graphics.path $docxsl/images/ \
-        -param highlight.source 1 \
-        -pdf $target/../../distributions/manual.$lang.pdf
+for po in src/main/po/*.po; do
+    lang=`basename ${po/.po/}`
+    xml2po -u $po src/main/reference/*.xml
 done
 
