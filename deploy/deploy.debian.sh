379c7f7aa8f367f290790dad5ccd109d26c5a405
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2016-02-17 22:53:05 +0300

    Deb: remove dput usage

--- deploy/deploy.debian.sh
+++ deploy/deploy.debian.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+cd `dirname $0`/..
 mkdir -p ~/.ssh
-openssl aes-256-cbc -K $encrypted_1fe4e6dcebeb_key -iv $encrypted_1fe4e6dcebeb_iv -in deploy/id_rsa.enc -out ~/.ssh/id_rsa -d
-dput -c dput.cf -u deploy build/debPackage/*.changes
+openssl aes-256-cbc -K $encrypted_1fe4e6dcebeb_key -iv $encrypted_1fe4e6dcebeb_iv -in deploy/id_rsa.enc -out build/id_rsa -d
+chmod 600 build/id_rsa
+scp -i build/id_rsa -B -o StrictHostKeyChecking=no build/debPackage/git-as-svn* deploy@dist.bozaro.ru:incoming/
+rm build/id_rsa
--- deploy/deploy.manual.sh
+++ deploy/deploy.manual.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+cd `dirname $0`/..
 pip install ghp-import --user $USER &&
 $HOME/.local/bin/ghp-import -n build/doc &&
 git push -qf https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
