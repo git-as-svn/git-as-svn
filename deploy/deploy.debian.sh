b59e984591b67dbd9f6c2c587dc010fcca98fcfd
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2016-02-18 21:42:54 +0300

    Deb: Minor debian package upload improvement

--- deploy/deploy.debian.sh
+++ deploy/deploy.debian.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -e
+set -ex
 cd `dirname $0`/..
+
+function upload {
+    scp -i build/id_rsa -B -o StrictHostKeyChecking=no $@ deploy@dist.bozaro.ru:incoming/
+}
+
 mkdir -p ~/.ssh
 openssl aes-256-cbc -K $encrypted_1fe4e6dcebeb_key -iv $encrypted_1fe4e6dcebeb_iv -in deploy/id_rsa.enc -out build/id_rsa -d
 chmod 600 build/id_rsa
-scp -i build/id_rsa -B -o StrictHostKeyChecking=no build/debPackage/git-as-svn* deploy@dist.bozaro.ru:incoming/
+
+upload build/debPackage/*.dsc build/debPackage/*.tar.gz build/debPackage/*.deb
+upload build/debPackage/*.changes
+
 rm build/id_rsa
