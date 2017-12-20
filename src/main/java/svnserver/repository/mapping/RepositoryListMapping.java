git-as-svn/git-as-svn

commit 06a11acc5a293a9d86375f0c399e11d2e3d7d454

Fix repository mapping error (fix #122)

src/main/java/svnserver/repository/mapping/RepositoryListMapping.java
@@ -61,8 +61,9 @@ public class RepositoryListMapping implements VcsRepositoryMapping {
 
   @Nullable
   public static <T> Map.Entry<String, T> getMapped(@NotNull NavigableMap<String, T> mapping, @NotNull String prefix) {
-    final Map.Entry<String, T> entry = mapping.floorEntry(StringHelper.normalize(prefix));
-    if (entry != null && StringHelper.isParentPath(entry.getKey(), prefix)) {
+    final String path = StringHelper.normalizeDir(prefix);
+    final Map.Entry<String, T> entry = mapping.floorEntry(path);
+    if (entry != null && StringHelper.isParentPath(entry.getKey(), path)) {
       return entry;
     }
     return null;
@@ -79,7 +80,7 @@ public class RepositoryListMapping implements VcsRepositoryMapping {
     private final Map<String, VcsRepository> mapping = new TreeMap<>();
 
     public Builder add(@NotNull String prefix, @NotNull VcsRepository repository) {
-      mapping.put(StringHelper.normalize(prefix), repository);
+      mapping.put(StringHelper.normalizeDir(prefix), repository);
       return this;
     }
 

src/main/java/svnserver/StringHelper.java
@@ -67,6 +67,18 @@ public final class StringHelper {
   }
 
   @NotNull
+  public static String normalizeDir(@NotNull String path) {
+    if (path.isEmpty()) return "/";
+    String result = path;
+    if (result.charAt(0) != '/') {
+      result = "/" + result;
+    } else if (result.length() == 1) {
+      return "/";
+    }
+    return result.endsWith("/") ? result : (result + "/");
+  }
+
+  @NotNull
   public static String parentDir(@NotNull String fullPath) {
     int index = fullPath.lastIndexOf('/');
     return index >= 0 ? fullPath.substring(0, index) : "";
@@ -85,8 +97,16 @@ public final class StringHelper {
    * @return Returns true, if parentPath is base path of childPath.
    */
   public static boolean isParentPath(@NotNull String parentPath, @NotNull String childPath) {
-    return childPath.equals(parentPath)
-        || childPath.startsWith(parentPath) && (childPath.charAt(parentPath.length()) == '/');
+    if (childPath.startsWith(parentPath)) {
+      int parentLength = parentPath.length();
+      if (childPath.length() == parentLength)
+        return true;
+      if (childPath.charAt(parentLength) == '/')
+        return true;
+      if ((parentLength > 0) && (childPath.charAt(parentLength - 1) == '/'))
+        return true;
+    }
+    return false;
   }
 
   @Contract("null -> null; !null -> !null")

src/main/java/svnserver/ext/gitlab/mapping/GitLabMapping.java
@@ -74,7 +74,7 @@ public class GitLabMapping implements VcsRepositoryMapping {
   @Nullable
   public GitLabProject addRepository(@NotNull GitlabProject project) throws IOException, SVNException {
     final String projectName = project.getPathWithNamespace();
-    final String projectKey = StringHelper.normalize(projectName);
+    final String projectKey = StringHelper.normalizeDir(projectName);
     final GitLabProject oldProject = mapping.get(projectKey);
     if (oldProject == null || oldProject.getProjectId() != project.getId()) {
       final File basePath = ConfigHelper.joinPath(context.getBasePath(), config.getPath());
@@ -91,7 +91,7 @@ public class GitLabMapping implements VcsRepositoryMapping {
   }
 
   public void removeRepository(int projectId, @NotNull String projectName) {
-    final String projectKey = StringHelper.normalize(projectName);
+    final String projectKey = StringHelper.normalizeDir(projectName);
     final GitLabProject project = mapping.get(projectKey);
     if (project != null && project.getProjectId() == projectId) {
       if (mapping.remove(projectKey, project)) {

src/test/java/svnserver/repository/mapping/RepositoryListMappingTest.java
@@ -25,27 +25,30 @@ public class RepositoryListMappingTest {
   @Test
   public void testEmpty() {
     final TreeMap<String, String> map = new Builder()
-        .add("")
+        .add("/")
         .build();
-    checkMapped(map, "", "");
-    checkMapped(map, "", "/foo");
-    checkMapped(map, "", "/bar");
+    checkMapped(map, "/", "");
+    checkMapped(map, "/", "/foo");
+    checkMapped(map, "/", "/bar");
   }
 
   @Test
   public void testRepositoryByPrefix() {
     final TreeMap<String, String> map = new Builder()
-        .add("/foo")
-        .add("/bar")
+        .add("/foo/")
+        .add("/bar/")
+        .add("/foo.test/")
         .build();
     checkMapped(map, null, "");
     checkMapped(map, null, "/bazz");
     checkMapped(map, null, "/foo2");
     checkMapped(map, null, "/bar2");
-    checkMapped(map, "/foo", "/foo");
-    checkMapped(map, "/foo", "/foo/bar");
-    checkMapped(map, "/bar", "/bar");
-    checkMapped(map, "/bar", "/bar/foo");
+    checkMapped(map, "/foo/", "/foo");
+    checkMapped(map, "/foo/", "/foo/bar");
+    checkMapped(map, "/bar/", "/bar");
+    checkMapped(map, "/bar/", "/bar/foo");
+    checkMapped(map, "/foo.test/", "/foo.test");
+    checkMapped(map, "/foo.test/", "/foo.test/foo");
   }
 
   private void checkMapped(@NotNull NavigableMap<String, String> map, @Nullable String expected, @NotNull String prefix) {

commit 1b2b53754f00b4a1f53ec2b9dc4fc2d5c2f37e94

Fix git-as-svn unable to find prefix-mapped repositories
Broken by commit 06a11acc5a293a9d86375f0c399e11d2e3d7d454.

src/main/java/svnserver/repository/mapping/RepositoryListMapping.java
@@ -30,7 +30,11 @@ public final class RepositoryListMapping implements VcsRepositoryMapping {
   private final NavigableMap<String, VcsRepository> mapping;
 
   public RepositoryListMapping(@NotNull Map<String, VcsRepository> mapping) {
-    this.mapping = new TreeMap<>(mapping);
+    this.mapping = new TreeMap<>();
+
+    mapping.forEach((path, repository) -> {
+      RepositoryListMapping.this.mapping.put(StringHelper.normalizeDir(path), repository);
+    });
   }
 
   @Nullable

src/main/java/svnserver/server/command/DeltaCmd.java
@@ -178,9 +178,13 @@ public final class DeltaCmd extends BaseCmd<DeltaParams> {
 
     private class HeaderEntry implements AutoCloseable {
 
+      @NotNull
       private final SessionContext context;
+      @Nullable
       private final VcsFile file;
+      @NotNull
       private final HeaderWriter beginWriter;
+      @NotNull
       private final HeaderWriter endWriter;
       private boolean writed = false;
 

src/test/java/svnserver/SvnTestServer.java
@@ -152,7 +152,7 @@ public final class SvnTestServer implements SvnTester {
   @NotNull
   public static SvnTestServer createEmpty() throws Exception {
     final String branch = "master";
-    return new SvnTestServer(TestHelper.emptyRepository(), branch, "", false, null, true);
+    return new SvnTestServer(TestHelper.emptyRepository(), branch, "foo", false, null, true);
   }
 
   @NotNull
@@ -163,7 +163,7 @@ public final class SvnTestServer implements SvnTester {
 
   @NotNull
   public static SvnTestServer createMasterRepository() throws Exception {
-    return new SvnTestServer(new FileRepository(TestHelper.findGitPath()), null, "/master", true, null, true);
+    return new SvnTestServer(new FileRepository(TestHelper.findGitPath()), null, "master", true, null, true);
   }
 
   @Override