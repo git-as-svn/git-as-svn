git-as-svn/git-as-svn

commit daead0eacca0af280fce57d7c6db98feae2d74d1

Remove hand-written base64 decoding. Fixes #129

src/main/java/svnserver/auth/PlainAuthenticator.java
@@ -17,6 +17,7 @@ import svnserver.parser.SvnServerWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * @author Marat Radchenko <marat@slonopotamus.org>
@@ -39,7 +40,8 @@ public final class PlainAuthenticator implements Authenticator, PasswordChecker 
   @Nullable
   @Override
   public User authenticate(@NotNull SvnServerParser parser, @NotNull SvnServerWriter writer, @NotNull String token) throws IOException, SVNException {
-    final String[] credentials = new String(fromBase64(token), StandardCharsets.US_ASCII).split("\u0000");
+    final String decodedToken = new String(Base64.getDecoder().decode(token), StandardCharsets.US_ASCII);
+    final String[] credentials = decodedToken.split("\u0000");
     if (credentials.length < 3)
       return null;
 
@@ -52,33 +54,5 @@ public final class PlainAuthenticator implements Authenticator, PasswordChecker 
   @Override
   public User check(@NotNull String userName, @NotNull String password) throws SVNException, IOException {
     return passwordChecker.check(userName, password);
-  }
-
-  /**
-   * Taken from {@link org.tmatesoft.svn.core.internal.io.svn.sasl.SVNSaslAuthenticator#fromBase64(String)}.
-   */
-  private static byte[] fromBase64(@Nullable String src) {
-    if (src == null) {
-      return new byte[0];
-    }
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    for (int i = 0; i < src.length(); i++) {
-      char ch = src.charAt(i);
-      if (!Character.isWhitespace(ch) && ch != '\n' && ch != '\r') {
-        bos.write((byte) ch & 0xFF);
-      }
-    }
-    byte[] cbytes = new byte[src.length()];
-    src = new String(bos.toByteArray(), StandardCharsets.US_ASCII);
-    int clength = SVNBase64.base64ToByteArray(new StringBuffer(src), cbytes);
-    byte[] result = new byte[clength];
-    // strip trailing -1s.
-    for (int i = clength - 1; i >= 0; i--) {
-      if (i == -1) {
-        clength--;
-      }
-    }
-    System.arraycopy(cbytes, 0, result, 0, clength);
-    return result;
   }
 }

commit 793afd3865e4d45d59af78fd75da2f2c939823e7

Fix PLAIN authentication with native SVN client
Fixes #156. Broken by commit daead0eacca0af280fce57d7c6db98feae2d74d1.

src/main/java/svnserver/auth/PlainAuthenticator.java
@@ -38,7 +38,7 @@ public final class PlainAuthenticator implements Authenticator, PasswordChecker 
   @Nullable
   @Override
   public User authenticate(@NotNull SvnServerParser parser, @NotNull SvnServerWriter writer, @NotNull String token) throws IOException, SVNException {
-    final String decodedToken = new String(Base64.getDecoder().decode(token), StandardCharsets.US_ASCII);
+    final String decodedToken = new String(Base64.getDecoder().decode(token.trim()), StandardCharsets.US_ASCII);
     final String[] credentials = decodedToken.split("\u0000");
     if (credentials.length < 3)
       return null;

src/test/java/svnserver/SvnTestHelper.java
@@ -7,6 +7,7 @@
  */
 package svnserver;
 
+import org.apache.commons.lang.SystemUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.testng.Assert;
@@ -17,6 +18,7 @@ import org.tmatesoft.svn.core.io.diff.SVNDeltaGenerator;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -171,5 +173,20 @@ public final class SvnTestHelper {
       repo.getFile(filePath, repo.getLatestRevision(), null, stream);
       Assert.assertEquals(stream.toByteArray(), content);
     }
+  }
+
+  @Nullable
+  public static String findExecutable(@NotNull String name) {
+    final String path = System.getenv("PATH");
+    if (path != null) {
+      final String suffix = SystemUtils.IS_OS_WINDOWS ? ".exe" : "";
+      for (String dir : path.split(File.pathSeparator)) {
+        final File file = new File(dir, name + suffix);
+        if (file.exists()) {
+          return file.getAbsolutePath();
+        }
+      }
+    }
+    return null;
   }
 }

src/test/java/svnserver/ldap/AuthLdapTest.java
@@ -12,8 +12,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.tmatesoft.svn.core.SVNAuthenticationException;
 import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
+import svnserver.SvnTestHelper;
 import svnserver.SvnTestServer;
 import svnserver.auth.User;
 import svnserver.auth.UserDB;
@@ -30,6 +30,30 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @author Artem V. Navrotskiy (bozaro at buzzsoft.ru)
  */
 public class AuthLdapTest {
+  
+  /**
+   * Test for #156.
+   */
+  @Test
+  void nativeClient() throws Exception {
+    final String svn = SvnTestHelper.findExecutable("svn");
+    if (svn == null)
+      return;
+
+    try (
+        EmbeddedDirectoryServer ldap = EmbeddedDirectoryServer.create();
+        SvnTestServer server = SvnTestServer.createEmpty(ldap.createUserConfig(), false)
+    ) {
+      final String[] command = {svn, "--non-interactive", "ls", "--username=ldapadmin", "--password=ldapadmin", server.getUrl().toString()};
+      final int exitCode = new ProcessBuilder(command)
+          .redirectError(ProcessBuilder.Redirect.INHERIT)
+          .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+          .start()
+          .waitFor();
+      Assert.assertEquals(exitCode, 0);
+    }
+  }
+
   @Test
   public void validUser() throws Throwable {
     checkUser("ldapadmin", "ldapadmin");

src/test/java/svnserver/tester/SvnTesterExternalListener.java
@@ -7,7 +7,6 @@
  */
 package svnserver.tester;
 
-import org.apache.commons.lang.SystemUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -21,6 +20,7 @@ import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import svnserver.SvnTestHelper;
 import svnserver.TestHelper;
 
 import java.io.File;
@@ -84,8 +84,8 @@ public class SvnTesterExternalListener implements ITestListener {
   @Override
   public void onStart(ITestContext context) {
     try {
-      final String svnserve = findExecutable("svnserve");
-      final String svnadmin = findExecutable("svnadmin");
+      final String svnserve = SvnTestHelper.findExecutable("svnserve");
+      final String svnadmin = SvnTestHelper.findExecutable("svnadmin");
       if (svnserve != null && svnadmin != null) {
         log.warn("Native svn daemon executables: {}, {}", svnserve, svnadmin);
         daemon = new NativeDaemon(svnserve, svnadmin);
@@ -95,21 +95,6 @@ public class SvnTesterExternalListener implements ITestListener {
     } catch (Exception e) {
       throw new IllegalStateException(e);
     }
-  }
-
-  @Nullable
-  private static String findExecutable(@NotNull String name) {
-    final String path = System.getenv("PATH");
-    if (path != null) {
-      final String suffix = SystemUtils.IS_OS_WINDOWS ? ".exe" : "";
-      for (String dir : path.split(File.pathSeparator)) {
-        final File file = new File(dir, name + suffix);
-        if (file.exists()) {
-          return file.getAbsolutePath();
-        }
-      }
-    }
-    return null;
   }
 
   @Override