d2e91b1a59a6dac181b569e3cca34e8b63d23b52
Marat Radchenko <marat@slonopotamus.org>
2019-04-22 12:36:00 +0300

    Fix 'Dangling Javadoc comment' IDEA warning

--- src/test/java/svnserver/server/WCDepthTest.java
+++ src/test/java/svnserver/server/WCDepthTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of git-as-svn. It is subject to the license terms
  * in the LICENSE file found in the top-level directory of this distribution
  * and at http://www.gnu.org/licenses/gpl-2.0.html. No part of git-as-svn,
