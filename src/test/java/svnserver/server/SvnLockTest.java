git-as-svn/git-as-svn

commit b9212cd1975f42efd09fe25b560b63816f51883f

Path-based authentication
Heavily inspired by Subversion authz: http://svnbook.red-bean.com/nightly/en/svn.serverconfig.pathbasedauthz.html

src/main/java/svnserver/repository/VcsAccess.java
@@ -8,7 +8,6 @@
 package svnserver.repository;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.tmatesoft.svn.core.SVNErrorCode;
 import org.tmatesoft.svn.core.SVNErrorMessage;
 import org.tmatesoft.svn.core.SVNException;
@@ -26,9 +25,9 @@ import java.util.Map;
  */
 public interface VcsAccess extends Local {
 
-  default void checkRead(@NotNull User user, @Nullable String path) throws IOException, SVNException {
+  default void checkRead(@NotNull User user, @NotNull String path) throws IOException, SVNException {
     if (!canRead(user, path))
-      throw new SVNException(SVNErrorMessage.create(SVNErrorCode.RA_NOT_AUTHORIZED));
+      throw new SVNException(SVNErrorMessage.create(user.isAnonymous() ? SVNErrorCode.RA_NOT_AUTHORIZED : SVNErrorCode.AUTHZ_UNREADABLE));
   }
 
   /**
@@ -37,11 +36,11 @@ public interface VcsAccess extends Local {
    * @param user User.
    * @param path Checked path. If path is null - checks for at least some part of the repository.
    */
-  boolean canRead(@NotNull User user, @Nullable String path) throws IOException;
+  boolean canRead(@NotNull User user, @NotNull String path) throws IOException;
 
-  default void checkWrite(@NotNull User user, @Nullable String path) throws IOException, SVNException {
+  default void checkWrite(@NotNull User user, @NotNull String path) throws IOException, SVNException {
     if (!canWrite(user, path))
-      throw new SVNException(SVNErrorMessage.create(SVNErrorCode.RA_NOT_AUTHORIZED));
+      throw new SVNException(SVNErrorMessage.create(user.isAnonymous() ? SVNErrorCode.RA_NOT_AUTHORIZED : SVNErrorCode.AUTHZ_UNWRITABLE));
   }
 
   /**
@@ -50,7 +49,7 @@ public interface VcsAccess extends Local {
    * @param user User.
    * @param path Checked path. If path is null - checks for at least some part of the repository.
    */
-  boolean canWrite(@NotNull User user, @Nullable String path) throws IOException;
+  boolean canWrite(@NotNull User user, @NotNull String path) throws IOException;
 
   default void updateEnvironment(@NotNull Map<String, String> environment) {
   }

src/main/java/svnserver/server/SessionContext.java
@@ -105,11 +105,17 @@ public final class SessionContext {
     return server.isCompressionEnabled() && capabilities.contains("svndiff1");
   }
 
-  public void authenticate(boolean allowAnonymous) throws IOException, SVNException {
+  public void authenticate() throws IOException, SVNException {
     if (!user.isAnonymous())
       throw new IllegalStateException();
 
+    final boolean allowAnonymous = acl.canRead(user, getRepositoryPath(""));
     this.user = server.authenticate(this, allowAnonymous);
+  }
+
+  @NotNull
+  public String getRepositoryPath(@NotNull String localPath) {
+    return StringHelper.joinPath(parent, localPath);
   }
 
   @NotNull
@@ -147,11 +153,6 @@ public final class SessionContext {
     return getBranch().getRevisionInfo(rev).getFile(getRepositoryPath(path));
   }
 
-  @NotNull
-  public String getRepositoryPath(@NotNull String localPath) {
-    return StringHelper.joinPath(parent, localPath);
-  }
-
   @Nullable
   public GitFile getFile(int rev, @NotNull SVNURL url) throws SVNException, IOException {
     final String path = getRepositoryPath(url);
@@ -159,12 +160,16 @@ public final class SessionContext {
     return getBranch().getRevisionInfo(rev).getFile(path);
   }
 
-  public void checkRead(@Nullable String path) throws SVNException, IOException {
+  public void checkRead(@NotNull String path) throws SVNException, IOException {
     acl.checkRead(user, path);
   }
 
-  public void checkWrite(@Nullable String path) throws SVNException, IOException {
+  public void checkWrite(@NotNull String path) throws SVNException, IOException {
     acl.checkWrite(user, path);
+  }
+
+  public boolean canRead(@NotNull String path) throws IOException {
+    return acl.canRead(user, path);
   }
 
   public void skipUnsupportedCommand(@NotNull String cmd) throws IOException {

src/main/java/svnserver/server/SvnServer.java
@@ -27,7 +27,6 @@ import svnserver.parser.token.ListBeginToken;
 import svnserver.parser.token.ListEndToken;
 import svnserver.repository.RepositoryInfo;
 import svnserver.repository.RepositoryMapping;
-import svnserver.repository.VcsAccess;
 import svnserver.repository.git.GitBranch;
 import svnserver.server.command.*;
 import svnserver.server.msg.AuthReq;
@@ -72,7 +71,9 @@ public final class SvnServer extends Thread {
       SVNErrorCode.WC_NOT_UP_TO_DATE,
       SVNErrorCode.IO_WRITE_ERROR,
       SVNErrorCode.IO_PIPE_READ_ERROR,
-      SVNErrorCode.RA_SVN_REPOS_NOT_FOUND
+      SVNErrorCode.RA_SVN_REPOS_NOT_FOUND,
+      SVNErrorCode.AUTHZ_UNREADABLE,
+      SVNErrorCode.AUTHZ_UNWRITABLE
   )));
   @NotNull
   private static AtomicInteger threadNumber = new AtomicInteger(1);
@@ -207,7 +208,7 @@ public final class SvnServer extends Thread {
       return;
 
     final SessionContext context = new SessionContext(parser, writer, this, repositoryInfo, clientInfo);
-    context.authenticate(hasAnonymousAuthenticator(repositoryInfo));
+    context.authenticate();
     final GitBranch branch = context.getBranch();
     branch.updateRevisions();
     sendAnnounce(writer, repositoryInfo);
@@ -281,10 +282,6 @@ public final class SvnServer extends Thread {
       throw new SVNException(SVNErrorMessage.create(SVNErrorCode.VERSION_MISMATCH, "Unsupported protocol version: " + clientInfo.getProtocolVersion() + " (expected: 2)"));
     }
     return clientInfo;
-  }
-
-  private boolean hasAnonymousAuthenticator(RepositoryInfo repositoryInfo) throws IOException {
-    return repositoryInfo.getBranch().getRepository().getContext().sure(VcsAccess.class).canRead(User.getAnonymous(), null);
   }
 
   private void sendAnnounce(@NotNull SvnServerWriter writer, @NotNull RepositoryInfo repositoryInfo) throws IOException {

src/main/java/svnserver/server/step/CheckPermissionStep.java
@@ -49,7 +49,7 @@ public final class CheckPermissionStep implements Step {
         if (!context.getUser().isAnonymous()) {
           throw e;
         }
-        context.authenticate(false);
+        context.authenticate();
         checker.check(context);
         nextStep.process(context);
         return;

src/docs/asciidoc/examples/config.yml
@@ -31,20 +31,20 @@ cacheConfig: !persistentCache
   path: /var/cache/git-as-svn/git-as-svn.mapdb
 
 repositoryMapping: !listMapping
+  groups:
+    developers:
+      - test
   repositories:
     example:
-      acl:
-        groups:
-          - name: developers
-            users:
-              - test
-
-        access:
-          - path: /
-            # Use 'asterisk' (*) to allow access for all users
-            # use 'at' (@) for groups
-            allowed:
-              - '@developers'
+      access:
+        /:
+          # Asterisk (*) means all users
+          # $authenticated means authenticated users
+          # $anonymous means anonymous users
+          #
+          # Supported modes: r/rw/none. Empty string also means none
+          '*': r
+          '@developers': rw
       repository:
         # Allowed pushers:
         # !pushEmbedded - use embedded push (fast, no dependencies)

src/main/java/svnserver/auth/ACL.java
@@ -7,150 +7,196 @@
  */
 package svnserver.auth;
 
+import org.eclipse.collections.api.block.function.primitive.BooleanFunction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import svnserver.config.AclAccessConfig;
-import svnserver.config.AclConfig;
-import svnserver.config.GroupConfig;
+import svnserver.StringHelper;
+import svnserver.repository.RepositoryMapping;
 import svnserver.repository.VcsAccess;
 
 import java.util.*;
 
 /**
+ * This ACL reuses SVN's authz syntax as much as possible: http://svnbook.red-bean.com/nightly/en/svn.serverconfig.pathbasedauthz.html
+ *
  * @author Marat Radchenko <marat@slonopotamus.org>
  */
 public final class ACL implements VcsAccess {
   @NotNull
-  public static final String EveryoneMarker = "*";
-
+  static final String EveryoneMarker = "*";
   @NotNull
-  private final Set<String> groups = new HashSet<>();
+  static final String AnonymousMarker = "$anonymous";
+  @NotNull
+  static final String AuthenticatedMarker = "$authenticated";
+  @NotNull
+  private static final String GroupPrefix = "@";
+
   @NotNull
   private final Map<String, Set<String>> user2groups = new HashMap<>();
+  @NotNull
+  private final Set<String> anonymousGroups = new HashSet<>();
+  @NotNull
+  private final Set<String> authenticatedGroups = new HashSet<>();
 
   @NotNull
-  private final Map<String, Set<AclEntry>> path2acl = new HashMap<>();
-  private final boolean anonymousRead;
+  private final NavigableMap<String, Map<ACLEntry, AccessMode>> path2acl = new TreeMap<>();
 
-  public ACL(@NotNull AclConfig config) {
-    anonymousRead = config.isAnonymousRead();
-    for (final GroupConfig group : config.getGroups()) {
-      final String name = group.getName();
+  public ACL(@NotNull Map<String, String[]> groups, @NotNull Map<String, Map<String, String>> access) {
+    for (final Map.Entry<String, String[]> group : groups.entrySet()) {
+      final String name = group.getKey();
 
       if (name.isEmpty())
         throw new IllegalArgumentException("Group with empty name is not a good idea");
 
-      if (group.getUsers().length == 0)
+      if (group.getValue().length == 0)
         throw new IllegalArgumentException("Group is empty: " + name);
 
-      if (!groups.add(name))
-        throw new IllegalArgumentException("Duplicate group found: " + name);
-
-      for (String user : group.getUsers())
-        user2groups.computeIfAbsent(user, s -> new HashSet<>()).add(name);
+      for (String member : group.getValue())
+        if (member.equals(AnonymousMarker))
+          anonymousGroups.add(name);
+        else if (member.equals(AuthenticatedMarker))
+          authenticatedGroups.add(name);
+        else if (member.startsWith(GroupPrefix))
+          throw new IllegalArgumentException("Groups of groups are not supported yet: " + name);
+        else
+          user2groups.computeIfAbsent(member, s -> new HashSet<>()).add(name);
     }
 
-    if (config.getAccess().length == 0)
-      throw new IllegalArgumentException("Empty ACL");
-
-    for (AclAccessConfig access : config.getAccess()) {
-      final String path = access.getPath();
+    for (Map.Entry<String, Map<String, String>> pathEntry : access.entrySet()) {
+      final String path = pathEntry.getKey();
 
       if (!path.startsWith("/"))
-        throw new IllegalArgumentException("ACL must start with slash (/): " + path);
+        throw new IllegalArgumentException("ACL entry must start with slash (/): " + path);
 
       if (path.endsWith("/") && path.length() > 1)
-        throw new IllegalArgumentException("ACL must not end with slash (/): " + path);
+        throw new IllegalArgumentException("ACL entry must not end with slash (/): " + path);
 
-      if (access.getAllowed().length == 0)
-        throw new IllegalArgumentException("ACL is empty: " + path);
+      if (pathEntry.getValue().isEmpty())
+        throw new IllegalArgumentException("ACL entry is empty: " + path);
 
-      if (path2acl.get(path) != null)
-        throw new IllegalArgumentException("Duplicate ACL: " + path);
-
-      for (String allowed : access.getAllowed())
-        addAccess(path, allowed);
+      for (Map.Entry<String, String> aclEntry : pathEntry.getValue().entrySet()) {
+        final AccessMode accessMode = AccessMode.fromString(aclEntry.getValue());
+        addAclEntry(path, aclEntry.getKey(), accessMode, groups.keySet());
+      }
     }
   }
 
-  private void addAccess(@NotNull String path, @NotNull String allowed) {
-    final AclEntry entry;
-    if (allowed.equals(EveryoneMarker))
-      entry = new EveryoneAclEntry();
-    else if (allowed.startsWith("@")) {
-      final String group = allowed.substring(1);
+  private void addAclEntry(@NotNull String path, @NotNull String entryString, @NotNull AccessMode accessMode, @NotNull Set<String> allGroups) {
+    final ACLEntry entry;
+    if (entryString.equals(EveryoneMarker))
+      entry = EveryoneACLEntry.instance;
+    else if (entryString.equals(AnonymousMarker))
+      entry = AnonymousACLEntry.instance;
+    else if (entryString.equals(AuthenticatedMarker))
+      entry = AuthenticatedACLEntry.instance;
+    else if (entryString.startsWith(GroupPrefix)) {
+      final String group = entryString.substring(1);
 
-      if (!groups.contains(group))
+      if (!allGroups.contains(group))
         throw new IllegalArgumentException("ACL entry " + path + " uses unknown group: " + group);
 
-      entry = new GroupAclEntry(group);
-    } else {
-      entry = new UserAclEntry(allowed);
+      entry = new GroupACLEntry(group);
+    } else
+      entry = new UserACLEntry(entryString);
+
+    if (path2acl.computeIfAbsent(StringHelper.normalizeDir(path), s -> new HashMap<>()).put(entry, accessMode) != null)
+      throw new IllegalArgumentException("Duplicate ACL entry " + path + ": " + entryString);
+  }
+
+  @Override
+  public boolean canRead(@NotNull User user, @NotNull String path) {
+    return doCheck(user, path, AccessMode::allowsRead);
+  }
+
+  @Override
+  public boolean canWrite(@NotNull User user, @NotNull String path) {
+    return doCheck(user, path, AccessMode::allowsWrite);
+  }
+
+  private boolean doCheck(@NotNull User user, @NotNull String path, @NotNull BooleanFunction<AccessMode> checker) {
+    String pathToCheck = path;
+
+    while (true) {
+      final Map.Entry<String, Map<ACLEntry, AccessMode>> pathEntry = RepositoryMapping.getMapped(path2acl, pathToCheck);
+
+      if (pathEntry == null)
+        break;
+
+      final CheckResult checkResult = check(user, checker, pathEntry.getValue());
+      if (checkResult == CheckResult.Deny)
+        return false;
+      else if (checkResult == CheckResult.Allow)
+        return true;
+
+      // We didn't find a matching entry, so need to go up in hierarchy
+      final int prevSlash = pathEntry.getKey().lastIndexOf('/', pathEntry.getKey().length() - 2);
+      if (prevSlash < 0)
+        break;
+      pathToCheck = pathEntry.getKey().substring(0, prevSlash);
     }
 
-    if (!path2acl.computeIfAbsent(path, s -> new HashSet<>()).add(entry))
-      throw new IllegalArgumentException("Duplicate ACL entry " + path + ": " + allowed);
+    return false;
   }
 
-  private interface AclEntry {
-    boolean allows(@NotNull String user);
+  @NotNull
+  private ACL.CheckResult check(@NotNull User user, @NotNull BooleanFunction<AccessMode> checker, @NotNull Map<ACLEntry, AccessMode> entries) {
+    CheckResult result = CheckResult.Unspecified;
+
+    for (Map.Entry<ACLEntry, AccessMode> aclEntry : entries.entrySet()) {
+      if (!aclEntry.getKey().matches(user))
+        continue;
+
+      if (checker.booleanValueOf(aclEntry.getValue()))
+        // Explicit allow is stronger than explicit deny
+        return CheckResult.Allow;
+
+      result = CheckResult.Deny;
+    }
+
+    return result;
   }
 
-  private class GroupAclEntry implements AclEntry {
+  private enum CheckResult {
+    Unspecified,
+    Allow,
+    Deny
+  }
+
+  private enum AccessMode {
+    none,
+    r,
+    rw;
+
     @NotNull
-    private final String group;
+    static AccessMode fromString(@Nullable String value) {
+      if (value == null || value.isEmpty())
+        return none;
 
-    private GroupAclEntry(@NotNull String group) {
-      this.group = group;
+      return valueOf(value);
     }
 
-    @Override
-    public boolean allows(@NotNull String user) {
-      return user2groups.getOrDefault(user, Collections.emptySet()).contains(group);
+    boolean allowsRead() {
+      return this != none;
     }
 
-    @Override
-    public int hashCode() {
-      return group.hashCode();
-    }
-
-    @Override
-    public boolean equals(@Nullable Object o) {
-      return o instanceof GroupAclEntry && group.equals(((GroupAclEntry) o).group);
+    boolean allowsWrite() {
+      return this == rw;
     }
   }
 
-  private class UserAclEntry implements AclEntry {
+  private interface ACLEntry {
+    boolean matches(@NotNull User user);
+  }
+
+  private static final class EveryoneACLEntry implements ACLEntry {
     @NotNull
-    private final String user;
+    private static final ACLEntry instance = new EveryoneACLEntry();
 
-    private UserAclEntry(@NotNull String user) {
-      this.user = user;
+    private EveryoneACLEntry() {
     }
 
     @Override
-    public boolean allows(@NotNull String user) {
-      return user.equals(this.user);
-    }
-
-    @Override
-    public int hashCode() {
-      return user.hashCode();
-    }
-
-    @Override
-    public boolean equals(@Nullable Object o) {
-      return o instanceof UserAclEntry && user.equals(((UserAclEntry) o).user);
-    }
-  }
-
-  private class EveryoneAclEntry implements AclEntry {
-
-    private EveryoneAclEntry() {
-    }
-
-    @Override
-    public boolean allows(@NotNull String user) {
+    public boolean matches(@NotNull User user) {
       return true;
     }
 
@@ -161,40 +207,107 @@ public final class ACL implements VcsAccess {
 
     @Override
     public boolean equals(@Nullable Object o) {
-      return o instanceof EveryoneAclEntry;
+      return o instanceof EveryoneACLEntry;
     }
   }
 
-  private boolean doCheck(@NotNull User user, @NotNull String path) {
-    for (AclEntry entry : path2acl.getOrDefault(path, Collections.emptySet()))
-      if (entry.allows(user.getUserName()))
-        return true;
+  private static final class UserACLEntry implements ACLEntry {
+    @NotNull
+    private final String user;
 
-    return false;
-  }
-
-  @Override
-  public boolean canWrite(@NotNull User user, @Nullable String path) {
-    return !user.isAnonymous() && canRead(user, path);
-  }
-
-  @Override
-  public boolean canRead(@NotNull User user, @Nullable String path) {
-    if (user.isAnonymous() && !anonymousRead)
-      return false;
-
-    if (path == null)
-      return true;
-
-    String toCheck = path;
-
-    while (!toCheck.isEmpty()) {
-      if (doCheck(user, toCheck))
-        return true;
-
-      toCheck = toCheck.substring(0, toCheck.lastIndexOf('/'));
+    private UserACLEntry(@NotNull String user) {
+      this.user = user;
     }
 
-    return doCheck(user, "/");
+    @Override
+    public boolean matches(@NotNull User user) {
+      return !user.isAnonymous() && user.getUserName().equals(this.user);
+    }
+
+    @Override
+    public int hashCode() {
+      return user.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+      return o instanceof UserACLEntry && user.equals(((UserACLEntry) o).user);
+    }
+  }
+
+  private static final class AnonymousACLEntry implements ACLEntry {
+    @NotNull
+    private static final ACLEntry instance = new AnonymousACLEntry();
+
+    private AnonymousACLEntry() {
+    }
+
+    @Override
+    public boolean matches(@NotNull User user) {
+      return user.isAnonymous();
+    }
+
+    @Override
+    public int hashCode() {
+      return getClass().hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+      return o instanceof AnonymousACLEntry;
+    }
+  }
+
+  private static final class AuthenticatedACLEntry implements ACLEntry {
+    @NotNull
+    private static final ACLEntry instance = new AuthenticatedACLEntry();
+
+    private AuthenticatedACLEntry() {
+    }
+
+    @Override
+    public boolean matches(@NotNull User user) {
+      return !user.isAnonymous();
+    }
+
+    @Override
+    public int hashCode() {
+      return getClass().hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+      return o instanceof AuthenticatedACLEntry;
+    }
+  }
+
+  private final class GroupACLEntry implements ACLEntry {
+    @NotNull
+    private final String group;
+
+    private GroupACLEntry(@NotNull String group) {
+      this.group = group;
+    }
+
+    @Override
+    public boolean matches(@NotNull User user) {
+      if (user.isAnonymous())
+        return anonymousGroups.contains(group);
+
+      if (authenticatedGroups.contains(group))
+        return true;
+
+      return user2groups.getOrDefault(user.getUserName(), Collections.emptySet()).contains(group);
+    }
+
+    @Override
+    public int hashCode() {
+      return group.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+      return o instanceof GroupACLEntry && group.equals(((GroupACLEntry) o).group);
+    }
   }
 }

src/main/java/svnserver/auth/User.java
@@ -20,7 +20,7 @@ import java.util.Objects;
  */
 public final class User {
   @NotNull
-  private static final User anonymousUser = new User("anonymous", "anonymous", null, null, true);
+  private static final User anonymousUser = new User("$anonymous", "anonymous", null, null, true);
 
   private final boolean isAnonymous;
   @NotNull

src/main/java/svnserver/config/AclConfig.java
@@ -1,57 +1,0 @@
-/*
- * This file is part of git-as-svn. It is subject to the license terms
- * in the LICENSE file found in the top-level directory of this distribution
- * and at http://www.gnu.org/licenses/gpl-2.0.html. No part of git-as-svn,
- * including this file, may be copied, modified, propagated, or distributed
- * except according to the terms contained in the LICENSE file.
- */
-package svnserver.config;
-
-import org.jetbrains.annotations.NotNull;
-import svnserver.auth.ACL;
-import svnserver.repository.VcsAccess;
-
-/**
- * @author Marat Radchenko <marat@slonopotamus.org>
- */
-public final class AclConfig {
-
-  @NotNull
-  private GroupConfig[] groups = GroupConfig.emptyArray;
-
-  @NotNull
-  private AclAccessConfig[] access = new AclAccessConfig[]{new AclAccessConfig("/", new String[]{ACL.EveryoneMarker})};
-
-  private boolean anonymousRead;
-
-  public AclConfig() {
-    this(false);
-  }
-
-  public AclConfig(boolean anonymousRead) {
-    this.anonymousRead = anonymousRead;
-  }
-
-  @NotNull
-  public GroupConfig[] getGroups() {
-    return groups;
-  }
-
-  @NotNull
-  public AclAccessConfig[] getAccess() {
-    return access;
-  }
-
-  public boolean isAnonymousRead() {
-    return anonymousRead;
-  }
-
-  public void setAnonymousRead(boolean anonymousRead) {
-    this.anonymousRead = anonymousRead;
-  }
-
-  @NotNull
-  public VcsAccess create() {
-    return new ACL(this);
-  }
-}

commit 1141990a2b279ab26495a6045c3f0fd1745607b8

Fix broken anonymous -> authenticated auth upgrade
Plus, add a test for it.

Broken by commit b9212cd1975f42efd09fe25b560b63816f51883f

src/main/java/svnserver/repository/VcsAccess.java
@@ -39,8 +39,11 @@ public interface VcsAccess extends Local {
   boolean canRead(@NotNull User user, @NotNull String path) throws IOException;
 
   default void checkWrite(@NotNull User user, @NotNull String path) throws IOException, SVNException {
+    if (user.isAnonymous())
+      throw new SVNException(SVNErrorMessage.create(SVNErrorCode.RA_NOT_AUTHORIZED));
+
     if (!canWrite(user, path))
-      throw new SVNException(SVNErrorMessage.create(user.isAnonymous() ? SVNErrorCode.RA_NOT_AUTHORIZED : SVNErrorCode.AUTHZ_UNWRITABLE));
+      throw new SVNException(SVNErrorMessage.create(SVNErrorCode.AUTHZ_UNWRITABLE));
   }
 
   /**

src/main/java/svnserver/server/SessionContext.java
@@ -105,11 +105,11 @@ public final class SessionContext {
     return server.isCompressionEnabled() && capabilities.contains("svndiff1");
   }
 
-  public void authenticate() throws IOException, SVNException {
+  public void authenticate(boolean allowAnonymous) throws IOException, SVNException {
     if (!user.isAnonymous())
       throw new IllegalStateException();
 
-    final boolean allowAnonymous = acl.canRead(user, getRepositoryPath(""));
+    allowAnonymous &= acl.canRead(user, getRepositoryPath(""));
     this.user = server.authenticate(this, allowAnonymous);
   }
 

src/main/java/svnserver/server/SvnServer.java
@@ -208,7 +208,7 @@ public final class SvnServer extends Thread {
       return;
 
     final SessionContext context = new SessionContext(parser, writer, this, repositoryInfo, clientInfo);
-    context.authenticate();
+    context.authenticate(true);
     final GitBranch branch = context.getBranch();
     branch.updateRevisions();
     sendAnnounce(writer, repositoryInfo);

src/main/java/svnserver/server/step/CheckPermissionStep.java
@@ -49,7 +49,7 @@ public final class CheckPermissionStep implements Step {
         if (!context.getUser().isAnonymous()) {
           throw e;
         }
-        context.authenticate();
+        context.authenticate(false);
         checker.check(context);
         nextStep.process(context);
         return;

src/test/java/svnserver/server/SvnLockTest.java
@@ -18,6 +18,7 @@ import org.tmatesoft.svn.core.io.ISVNEditor;
 import org.tmatesoft.svn.core.io.ISVNLockHandler;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import svnserver.StringHelper;
+import svnserver.SvnTestServer;
 import svnserver.tester.SvnTester;
 import svnserver.tester.SvnTesterDataProvider;
 import svnserver.tester.SvnTesterExternalListener;
@@ -323,6 +324,16 @@ public class SvnLockTest {
         editor.closeEdit();
       }
       Assert.assertNull(repo.getLock("/example.txt"));
+    }
+  }
+
+  @Test
+  public void lockWithDelayedAuth() throws Exception {
+    try (SvnTestServer server = SvnTestServer.createEmpty(null, true)) {
+      final SVNRepository repo = server.openSvnRepository();
+      createFile(repo, "/example.txt", "", propsEolNative);
+      final SVNLock lock = lock(repo, "/example.txt", repo.getLatestRevision(), false, null);
+      Assert.assertNotNull(lock);
     }
   }
 