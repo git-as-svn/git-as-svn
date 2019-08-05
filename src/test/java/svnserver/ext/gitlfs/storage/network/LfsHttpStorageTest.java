git-as-svn/git-as-svn

commit 244b0352f4f32ebb8c0463abf43788cb0ccee6f9

Stop writing files to disk when downloading them from remote LFS

src/main/java/svnserver/TemporaryOutputStream.java
@@ -7,7 +7,6 @@
  */
 package svnserver;
 
-import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.TestOnly;
@@ -34,9 +33,11 @@ public final class TemporaryOutputStream extends OutputStream {
   @Nullable
   private FileOutputStream fileOutputStream;
   private long totalSize = 0;
+
   public TemporaryOutputStream() {
     this(MAX_MEMORY_SIZE);
   }
+
   public TemporaryOutputStream(int maxMemorySize) {
     this.maxMemorySize = maxMemorySize;
     this.holder = new FileHolder(this::cleanup);
@@ -46,11 +47,6 @@ public final class TemporaryOutputStream extends OutputStream {
     if (file != null && !file.delete()) {
       throw new IOException("Can't delete temporary file: " + file.getAbsolutePath());
     }
-  }
-
-  public TemporaryOutputStream(@NotNull InputStream stream) throws IOException {
-    this(MAX_MEMORY_SIZE);
-    IOUtils.copy(stream, this);
   }
 
   @Override

src/main/java/svnserver/ext/gitlfs/storage/network/LfsHttpStorage.java
@@ -18,7 +18,6 @@ import ru.bozaro.gitlfs.common.LockConflictException;
 import ru.bozaro.gitlfs.common.VerifyLocksResult;
 import ru.bozaro.gitlfs.common.data.*;
 import svnserver.Loggers;
-import svnserver.TemporaryOutputStream;
 import svnserver.auth.User;
 import svnserver.ext.gitlfs.storage.LfsReader;
 import svnserver.ext.gitlfs.storage.LfsStorage;
@@ -77,7 +76,7 @@ public abstract class LfsHttpStorage implements LfsStorage {
   @NotNull
   final InputStream getObject(@NotNull Links links) throws IOException {
     final Client lfsClient = lfsClient(User.getAnonymous());
-    return lfsClient.getObject(null, links, TemporaryOutputStream::new).toInputStream();
+    return lfsClient.getObject(null, links, inputStream -> inputStream);
   }
 
   @Override

src/main/java/svnserver/repository/git/GitDeltaConsumer.java
@@ -20,6 +20,7 @@ import org.tmatesoft.svn.core.SVNErrorCode;
 import org.tmatesoft.svn.core.SVNErrorMessage;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNProperty;
+import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
 import org.tmatesoft.svn.core.io.ISVNDeltaConsumer;
 import org.tmatesoft.svn.core.io.diff.SVNDeltaProcessor;
 import org.tmatesoft.svn.core.io.diff.SVNDiffWindow;
@@ -28,7 +29,10 @@ import svnserver.TemporaryOutputStream;
 import svnserver.auth.User;
 import svnserver.repository.git.filter.GitFilter;
 
-import java.io.*;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -159,7 +163,7 @@ public final class GitDeltaConsumer implements ISVNDeltaConsumer {
       newFilter = writer.getBranch().getRepository().getFilter(props.containsKey(SVNProperty.SPECIAL) ? FileMode.SYMLINK : FileMode.REGULAR_FILE, entry.getRawProperties());
       window = new SVNDeltaProcessor();
 
-      final InputStream base = (oldFilter != null && objectId != null) ? oldFilter.inputStream(objectId) : new ByteArrayInputStream(GitRepository.emptyBytes);
+      final InputStream base = (oldFilter != null && objectId != null) ? oldFilter.inputStream(objectId) : SVNFileUtil.DUMMY_IN;
       final OutputStream target = newFilter.outputStream(temporaryStream, user);
 
       window.applyTextDelta(base, new UncheckedCloseOutputStream(target), true);

commit 0796084308d9e420b62c9fab980cc9594400ebde

Fix downloading of large files from remote LFS + test
Broken by 244b0352f4f32ebb8c0463abf43788cb0ccee6f9

src/main/java/svnserver/TemporaryOutputStream.java
@@ -7,6 +7,7 @@
  */
 package svnserver;
 
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.TestOnly;
@@ -38,6 +39,11 @@ public final class TemporaryOutputStream extends OutputStream {
 
   public TemporaryOutputStream(int maxMemorySize) {
     this.maxMemorySize = maxMemorySize;
+  }
+
+  public TemporaryOutputStream(@NotNull InputStream stream) throws IOException {
+    this(MAX_MEMORY_SIZE);
+    IOUtils.copy(stream, this);
   }
 
   @Override

src/main/java/svnserver/ext/gitlfs/storage/network/LfsHttpStorage.java
@@ -18,6 +18,7 @@ import ru.bozaro.gitlfs.common.LockConflictException;
 import ru.bozaro.gitlfs.common.VerifyLocksResult;
 import ru.bozaro.gitlfs.common.data.*;
 import svnserver.Loggers;
+import svnserver.TemporaryOutputStream;
 import svnserver.auth.User;
 import svnserver.ext.gitlfs.storage.LfsReader;
 import svnserver.ext.gitlfs.storage.LfsStorage;
@@ -76,7 +77,7 @@ public abstract class LfsHttpStorage implements LfsStorage {
   @NotNull
   final InputStream getObject(@NotNull Links links) throws IOException {
     final Client lfsClient = lfsClient(User.getAnonymous());
-    return lfsClient.getObject(null, links, inputStream -> inputStream);
+    return lfsClient.getObject(null, links, TemporaryOutputStream::new).toInputStream();
   }
 
   @Override

src/test/java/svnserver/ext/gitlfs/storage/network/LfsHttpStorageTest.java
@@ -7,7 +7,8 @@
  */
 package svnserver.ext.gitlfs.storage.network;
 
-import com.google.common.io.CharStreams;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteStreams;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
@@ -55,12 +56,12 @@ import svnserver.repository.VcsAccess;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import static svnserver.server.SvnFilePropertyTest.propsBinary;
 
 /**
  * Simple test for LfsLocalStorage.
@@ -98,13 +99,19 @@ public final class LfsHttpStorageTest {
         final SVNRepository svnRepository = server.openSvnRepository();
         SvnTestHelper.createFile(svnRepository, ".gitattributes", "* -text\n*.txt filter=lfs diff=lfs merge=lfs -text", null);
 
-        SvnTestHelper.createFile(svnRepository, "1.txt", "some text", null);
-        SvnTestHelper.checkFileContent(svnRepository, "1.txt", "some text");
+        // 10 MB
+        final byte[] data = new byte[10 * 1024 * 1024];
+        for (int i = 0; i < data.length; ++i) {
+          data[i] = (byte) (i % 256);
+        }
+
+        SvnTestHelper.createFile(svnRepository, "1.txt", data, propsBinary);
+        SvnTestHelper.checkFileContent(svnRepository, "1.txt", data);
 
         Assert.assertEquals(backendStorage.getFiles().size(), 1);
         final byte[] lfsBytes = backendStorage.getFiles().values().iterator().next();
         Assert.assertNotNull(lfsBytes);
-        Assert.assertEquals(new String(lfsBytes, StandardCharsets.UTF_8), "some text");
+        Assert.assertEquals(lfsBytes, data);
 
         final Holder<SVNLock> lockHolder = new Holder<>(null);
         svnRepository.lock(Collections.singletonMap("1.txt", svnRepository.getLatestRevision()), null, false, new ISVNLockHandler() {
@@ -147,32 +154,41 @@ public final class LfsHttpStorageTest {
       // Register storage
       sharedContext.sure(LfsServer.class).register(localContext, localContext.sure(LfsStorage.class));
 
+      // 10 MB
+      final byte[] data = new byte[10 * 1024 * 1024];
+      for (int i = 0; i < data.length; ++i) {
+        data[i] = (byte) (i % 256);
+      }
+
+      final String oid = "sha256:" + Hashing.sha256().hashBytes(data).toString();
+
       final URI url = webServer.getBaseUrl().resolve("example.git/").resolve(LfsServer.SERVLET_AUTH);
       final LfsHttpStorage storage = new GitAsSvnLfsHttpStorage(url);
 
       // Check file is not exists
-      Assert.assertNull(storage.getReader("sha256:61f27ddd5b4e533246eb76c45ed4bf4504daabce12589f97b3285e9d3cd54308", -1, user));
+      Assert.assertNull(storage.getReader(oid, -1, user));
 
       // Write new file
       try (final LfsWriter writer = storage.getWriter(user)) {
-        writer.write("Hello, world!!!".getBytes(StandardCharsets.UTF_8));
-        Assert.assertEquals(writer.finish(null), "sha256:61f27ddd5b4e533246eb76c45ed4bf4504daabce12589f97b3285e9d3cd54308");
+        writer.write(data);
+        Assert.assertEquals(writer.finish(null), oid);
       }
 
       // Write new file AGAIN
       try (final LfsWriter writer = storage.getWriter(user)) {
-        writer.write("Hello, world!!!".getBytes(StandardCharsets.UTF_8));
-        Assert.assertEquals(writer.finish(null), "sha256:61f27ddd5b4e533246eb76c45ed4bf4504daabce12589f97b3285e9d3cd54308");
+        writer.write(data);
+        Assert.assertEquals(writer.finish(null), oid);
       }
 
       // Read old file.
-      final LfsReader reader = storage.getReader("sha256:61f27ddd5b4e533246eb76c45ed4bf4504daabce12589f97b3285e9d3cd54308", -1, user);
+      final LfsReader reader = storage.getReader(oid, -1, user);
       Assert.assertNotNull(reader);
       Assert.assertNull(reader.getMd5());
-      Assert.assertEquals(reader.getSize(), 15);
+      Assert.assertEquals(reader.getSize(), data.length);
 
       try (final InputStream stream = reader.openStream()) {
-        Assert.assertEquals(CharStreams.toString(new InputStreamReader(stream, StandardCharsets.UTF_8)), "Hello, world!!!");
+        final byte[] actual = ByteStreams.toByteArray(stream);
+        Assert.assertEquals(actual, data);
       }
     }
   }

src/test/java/svnserver/server/SvnFilePropertyTest.java
@@ -32,6 +32,10 @@ import static svnserver.SvnTestHelper.*;
  */
 public class SvnFilePropertyTest {
   @NotNull
+  public final static Map<String, String> propsBinary = ImmutableMap.<String, String>builder()
+      .put(SVNProperty.MIME_TYPE, SVNFileUtil.BINARY_MIME_TYPE)
+      .build();
+  @NotNull
   private final static Map<String, String> propsEolNative = ImmutableMap.<String, String>builder()
       .put(SVNProperty.EOL_STYLE, SVNProperty.EOL_STYLE_NATIVE)
       .build();
@@ -53,18 +57,12 @@ public class SvnFilePropertyTest {
       .put(SVNProperty.INHERITABLE_AUTO_PROPS, "*.txt = svn:eol-style=LF\n")
       .build();
   @NotNull
-  private final static Map<String, String> propsBinary = ImmutableMap.<String, String>builder()
-      .put(SVNProperty.MIME_TYPE, SVNFileUtil.BINARY_MIME_TYPE)
-      .build();
-  @NotNull
   private final static Map<String, String> propsNeedsLock = ImmutableMap.<String, String>builder()
       .put(SVNProperty.NEEDS_LOCK, "*")
       .build();
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void executable() throws Exception {
@@ -99,8 +97,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void binary() throws Exception {
@@ -137,8 +133,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void symlink() throws Exception {
@@ -210,8 +204,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void symlinkBinary() throws Exception {
@@ -245,8 +237,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitUpdatePropertiesRoot() throws Exception {
@@ -275,8 +265,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitUpdatePropertiesSubdir() throws Exception {
@@ -321,8 +309,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitDirWithProperties() throws Exception {
@@ -348,8 +334,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitDirWithoutProperties() throws Exception {
@@ -377,8 +361,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitDirUpdateWithProperties() throws Exception {
@@ -418,8 +400,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitDirUpdateWithoutProperties() throws Exception {
@@ -461,8 +441,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitRootWithProperties() throws Exception {
@@ -488,8 +466,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitRootWithoutProperties() throws Exception {
@@ -517,8 +493,6 @@ public class SvnFilePropertyTest {
 
   /**
    * Check commit .gitattributes.
-   *
-   * @throws Exception
    */
   @Test
   public void commitFileWithProperties() throws Exception {