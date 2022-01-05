c8ca04ff3839c5453645189d31929911042b757f
Marat Radchenko <marat@slonopotamus.org>
2022-01-05 18:28:25 +0300

    Update Gradle to 7.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 tasks.wrapper {
-    gradleVersion = "7.3"
+    gradleVersion = "7.3.3"
     distributionType = Wrapper.DistributionType.ALL
 }
 
